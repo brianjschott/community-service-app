c74b1f6de934459f3074e6c36591b729c5c25bc5
Brian <brianjschott@gmail.com>
2019-03-29 15:30:43 -0400

    Lets try this

--- src/Student.vue
+++ src/Student.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <h1>Student View</h1>
+    <div id="nav">
+      <a class="active" href="./home.html"><i class="material-icons">home</i></a>
+      <a href="./home.html"><i class="material-icons">show_chart</i></a>
+      <a href="./home.html">Ipsum</a>
+      <a href="./home.html">Dolor</a>
+      <a href="./home.html"><i class="material-icons">person</i></a>
+    </div>
+      
+    <div id="welcome student">
+      <h4>Welcome <!--This will be replaced with the student's name -->Student,</h4>
+    </div>
+    <div id="piechart">
+    </div>
   </div>
 </template>
 
