5edcfe9ae7524e9900055ee5e3f83270022eca6d
elijahbrowne1 <46696588+elijahbrowne1@users.noreply.github.com>
2019-04-14 19:47:31 -0400

    Add files via upload

---
+++ form.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    M.updateTextFields();
+  });
+
+/*
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.timepicker');
+    var instances = M.Timepicker.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.timepicker').timepicker();
+  });*/
