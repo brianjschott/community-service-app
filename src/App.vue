d17ec0d63451d6600135032e73d16ab2f84bd1a8
Brian <brianjschott@gmail.com>
2019-04-20 17:01:06 -0400

    Cleaning out repeat code in multiple files

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,16 @@
 
 
 <script>
-import { initializeApp } from "firebase";
-import { studentRecordsref } from "./firebase";
+
 
 export default {
   data() {
     return {};
   },
   firebase: {
-    studentrecords: studentRecordsref
   },
   methods: {
-    submitData() {
-      studentRecordsref.push({
-        name: this.name,
-        hours: this.hour,
-        organization: this.organization
-      });
-    }
+    
   }
 };
 </script>
